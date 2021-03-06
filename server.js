5db969f4e85adf5ede038ad85d4a7c48784ac08b
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-03-07 00:52:59 +0900

    Add nodemon

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import dotenv from 'dotenv';
+import IndexRoute from './routes/index.route.js';
 import App from './app.js';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = new App();
+const app = new App([new IndexRoute()]);
 app.listen();
