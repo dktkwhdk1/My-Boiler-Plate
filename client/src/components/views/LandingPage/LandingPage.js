3f96a672dfbfeaf244f6a581cf42c6cc17ac8b15
강희석 <44192757+dktkwhdk1@users.noreply.github.com>
2021-03-10 19:16:26 +0900

    Add logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,21 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then(res => console.log(res));
   }, []);
+
+  const onLogoutHandler = () => {
+    axios.get('/api/users/logout').then(response => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃에 실패했습니다.');
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -16,6 +27,7 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onLogoutHandler}>로그아웃</button>
     </div>
   );
 }
